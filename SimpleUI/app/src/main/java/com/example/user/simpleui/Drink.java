b066b05218ac98f2cce69c0363f4efc130e1d0e6
singural <singural@gmail.com>
2016-07-14 10:51:20 +0800

    set Drink Data

---
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/Drink.java
@@ -0,0 +1,13 @@
+package com.example.user.simpleui;
+
+/**
+ * Created by Cindy on 2016/7/14.
+ */
+public class Drink {
+    String name;
+    int mPrice=0;
+    int lPrice=0;
+    int imageId;
+
+
+}
