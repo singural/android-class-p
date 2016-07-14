88ad44ec958855df9befa442a777bf3d88c497ab
singural <singural@gmail.com>
2016-07-14 10:36:28 +0800

    complete drink menu activity layout

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -3,14 +3,24 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class DrinkMenuActivity extends AppCompatActivity {
+
+    TextView totalTextView;
+    ListView drinkMenuListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_menu);
         Log.d("Debug","DrinkMenuActivity onCreate");
+
+        totalTextView=(TextView)findViewById(R.id.totalTextView);
+        drinkMenuListView=(ListView)findViewById(R.id.drinkMenuListView);
+
     }
 
     @Override
