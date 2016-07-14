99f0694618ef9411a575145ce505404bd25d2acd
singural <singural@gmail.com>
2016-07-14 12:08:48 +0800

    update total

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -1,8 +1,11 @@
 package com.example.user.simpleui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,6 +23,7 @@ public class DrinkMenuActivity extends AppCompatActivity {
     int[] imageId={R.drawable.drink1,R.drawable.drink2,R.drawable.drink3,R.drawable.drink4};
 
     List<Drink> drinks=new ArrayList<>();
+    List<Drink> orders=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +57,51 @@ public class DrinkMenuActivity extends AppCompatActivity {
     {
         DrinkAdapter adapter=new DrinkAdapter(this,drinks);
         drinkMenuListView.setAdapter(adapter);
+
+        //drinkMenuListView.setOnClickListener(new );
+/*
+        drinkMenuListView.setOnItemClickListener(new View.OnClickItemListener() {
+            @Override
+            public void onItemClick(View v) {
+                DrinkAdapter drinkAdapter=(DrinkAdapter)parent.getAdapter();
+                Drink drink=(Drink)drinkAdapter.getItem(position);
+                orders.add(drink);
+                updateTotal();
+
+            }
+        });
+        */
+
+        drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                      @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                              DrinkAdapter drinkAdapter = (DrinkAdapter)parent.getAdapter();
+                              Drink drink = (Drink)drinkAdapter.getItem(position);
+                             orders.add(drink);
+                            updateTotal();
+                            }
+                    });
+
+
     }
 
+    public void updateTotal()
+    {
+        int total=0;
+        for(Drink drink:orders)
+        {
+            total+=drink.mPrice;
+        }
+        totalTextView.setText(String.valueOf(total));
+    }
+
+    public void done(View view)
+    {
+        Intent intent=new Intent();
+
+        
+
+    }
     @Override
     protected void onStart() {
         super.onStart();
